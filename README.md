671ec2c79183d930bbd8942d2fc564695c68aa4f
Vadim Liventsev <hi@vadim.me>
2020-11-24 04:26:48 +0100

    Download script

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Cibi experiments
 
 Experiment specification files for [cibi](https://github.com/vadim0x60/cibi) benchmarks
+
+This repository contains everything you need to reproduce our experiments on your own hardware.
+To download the results we calculated (with all logs, etc.) run `python download.py`
---
+++ download.py
@@ -0,0 +1,14 @@
+from zipfile import ZipFile
+from urllib.request import urlopen
+from shutil import rmtree
+from io import BytesIO
+from os import path
+
+experiments_dir = path.dirname(__file__)
+for subdir in path.listdir(experiments_dir):
+    if path.isdir(subdir):
+        rmtree(subdir)
+
+resp = urlopen('https://surfdrive.surf.nl/files/index.php/s/QYQTxW0bScR4XnX/download')
+ZipFile(BytesIO(resp.read())).extractall(experiments_dir)
+
